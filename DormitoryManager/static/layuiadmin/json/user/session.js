e3321dfb13c0fd4aded2ca7db94fc1a60f60c9ed
昭雨薇姿 <69343069+AyagawaAsaba@users.noreply.github.com>
2024-11-19 16:46:21 +0800

    layuiadmin

---
+++ DormitoryManager/static/layuiadmin/json/user/session.js
@@ -0,0 +1,21 @@
+{
+    "code"
+:
+    0
+        , "msg"
+:
+    ""
+        , "data"
+:
+    {
+        "username"
+    :
+        "AyagawaAsaba"
+            , "sex"
+    :
+        "男"
+            , "role"
+    :
+        1
+    }
+}
---
+++ DormitoryManager/static/layuiadmin/json/useradmin/role.js
@@ -0,0 +1,50 @@
+{
+    "code"
+:
+    0
+        , "msg"
+:
+    ""
+        , "count"
+:
+    "100"
+        , "data"
+:
+    [{
+        "id": "1"
+        , "rolename": "超级管理员"
+        , "limits": "管理所有的管理员"
+        , "descr": "拥有至高无上的权利"
+        , "check": true
+    }, {
+        "id": "2"
+        , "rolename": "管理员"
+        , "limits": "所有列表的管理"
+        , "descr": "事情很多，权力很大"
+        , "check": true
+    }, {
+        "id": "3"
+        , "rolename": "教职工"
+        , "limits": "负责学校的日常"
+        , "descr": "教书育人"
+        , "check": false
+    }, {
+        "id": "4"
+        , "rolename": "辅导员"
+        , "limits": "负责学生的日常"
+        , "descr": "暂无"
+        , "check": false
+    }, {
+        "id": "5"
+        , "rolename": "学生"
+        , "limits": "学习知识"
+        , "descr": "3年一届的学生们"
+        , "check": false
+    }, {
+        "id": "6"
+        , "rolename": "访客"
+        , "limits": "对宿舍进行访问"
+        , "descr": "暂无"
+        , "check": false
+    }]
+}
